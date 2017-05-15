b212a786727417feef7d6bfc327ae9bf20c3ca3a
Brian OMeara <omeara.brian@gmail.com>
2017-05-15 16:25:58 -0400

    updates to do more taxa and include Co-PIs

--- FundingSearch/ParseNSFParameterized3.R
+++ FundingSearch/ParseNSFParameterized3.R
@@ -11,14 +11,17 @@ RunAnalysis <- function(focal.clade, data) {
   clade.info <- NULL
   try(clade.info <- NSFgrantparser::GetAllGrantDataForClades(c(focal.clade), data=data)[[1]])
   if(!is.null(clade.info)) {
-  save(clade.info, file=paste0("~/Desktop/GrantInfo_", focal.clade, ".rda"))
-  local.result <- data.frame(title=clade.info$Award.AwardTitle, amount=clade.info$Award.AwardAmount, date=clade.info$Award.AwardEffectiveDate, PI=clade.info$Award.Investigator.LastName, abstract=clade.info$Award.AbstractNarration, directorate=clade.info$Award.Organization.Directorate.LongName, division=clade.info$Award.Organization.Division.LongName, stringsAsFactors = FALSE)
-  write.csv(local.result, file=paste0("~/Desktop/GrantInfo_", focal.clade, ".csv"))
+  save(clade.info, file=paste0("GrantInfo_", focal.clade, ".zip"))
+  all.investigator.names.df <- apply(clade.info[,which(grepl("LastName", names(clade.info)))], 2, as.character)
+  all.investigator.names.vector <- unlist(all.investigator.names.df)
+  all.investigator.names.vector <- all.investigator.names.vector[!is.na(all.investigator.names.vector)]
+  local.result <- data.frame(title=clade.info$Award.AwardTitle, amount=clade.info$Award.AwardAmount, date=clade.info$Award.AwardEffectiveDate, PI=clade.info$Award.Investigator.LastName, abstract=clade.info$Award.AbstractNarration, directorate=clade.info$Award.Organization.Directorate.LongName, division=clade.info$Award.Organization.Division.LongName, investigators = paste(all.investigator.names.vector, collapse="|"), stringsAsFactors = FALSE)
+  write.csv(local.result, file=paste0("GrantInfo_", focal.clade, ".csv"))
   }
 }
 
 
-clades <- c("Hemiptera", "Odonata", "Blattodea", "Chelicerata", "Myriapoda", "Ascomycota", "Plantae", "Mammalia", "Coleoptera", "Basidiomycota", "Archaea", "Platyhelminthes", "Porifera", "Aves", "Nematoda", "Protozoa", "Tracheophyta", "Chromista", "Actinobacteria", "Lepidoptera", "Mollusca", "Annelida")
+clades <- c("Annelida", "Embryophyta", "Magnoliophyta", "Acrogymnospermae", "Polypodiopsida", "Bryophyta", "Plantae", "Hemiptera", "Odonata", "Blattodea", "Chelicerata", "Myriapoda", "Ascomycota", "Mammalia", "Coleoptera", "Basidiomycota", "Archaea", "Platyhelminthes", "Porifera", "Aves", "Nematoda", "Protozoa", "Tracheophyta", "Chromista", "Actinobacteria", "Lepidoptera", "Mollusca")
 
 
 
--- FundingSearch/ParseNSFbatcher.R
+++ FundingSearch/ParseNSFbatcher.R
@@ -1,11 +1,11 @@
 library(foreach)
 library(doParallel)
 
-clades <- c("Chelicerata", "Myriapoda", "Ascomycota", "Plantae", "Mammalia", "Coleoptera")
+clades <- c("Angiosperms", "Gymnosperms", "Pteridophytes", "Bryophytes", "Chelicerata", "Myriapoda", "Ascomycota", "Plantae", "Mammalia", "Coleoptera")
 setwd("~/Desktop")
 ExecuteMarkdown <- function(x) {
-  #rmarkdown::render("ParseNSFParameterized.Rmd", params = list(
-   rmarkdown::render("test.Rmd", params = list(
+  rmarkdown::render("ParseNSFParameterized3.Rmd", params = list(
+  # rmarkdown::render("test.Rmd", params = list(
       clade=x
   ))
 }
