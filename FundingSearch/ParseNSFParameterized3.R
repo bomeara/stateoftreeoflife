edfff9c1efd8ce5ce9cf88465aecbcb6684179f6
Brian OMeara <omeara.brian@gmail.com>
2017-05-15 16:48:44 -0400

    redoing run

--- FundingSearch/ParseNSFParameterized3.R
+++ FundingSearch/ParseNSFParameterized3.R
@@ -13,10 +13,12 @@ RunAnalysis <- function(focal.clade, data) {
   if(!is.null(clade.info)) {
   save(clade.info, file=paste0("GrantInfo_", focal.clade, ".zip"))
   all.investigator.names.df <- apply(clade.info[,which(grepl("LastName", names(clade.info)))], 2, as.character)
-  all.investigator.names.vector <- unlist(all.investigator.names.df)
-  all.investigator.names.vector <- all.investigator.names.vector[!is.na(all.investigator.names.vector)]
-  local.result <- data.frame(title=clade.info$Award.AwardTitle, amount=clade.info$Award.AwardAmount, date=clade.info$Award.AwardEffectiveDate, PI=clade.info$Award.Investigator.LastName, abstract=clade.info$Award.AbstractNarration, directorate=clade.info$Award.Organization.Directorate.LongName, division=clade.info$Award.Organization.Division.LongName, investigators = paste(all.investigator.names.vector, collapse="|"), stringsAsFactors = FALSE)
+  all.investigator.names.vector <- gsub('\\|NA', "", apply(all.investigator.names.df, 1, paste, collapse="|"))
+  local.result <- data.frame(title=clade.info$Award.AwardTitle, amount=clade.info$Award.AwardAmount, date=clade.info$Award.AwardEffectiveDate, PI=clade.info$Award.Investigator.LastName, abstract=clade.info$Award.AbstractNarration, directorate=clade.info$Award.Organization.Directorate.LongName, division=clade.info$Award.Organization.Division.LongName, investigators = all.investigator.names.vector, institution = clade.info$Award.Institution.Name, city=clade.info$Award.Institution.CityName, state=clade.info$Award.Institution.StateCode, zip=clade.info$Award.Institution.ZipCode, stringsAsFactors = FALSE)
   write.csv(local.result, file=paste0("GrantInfo_", focal.clade, ".csv"))
+  system(paste0("/usr/local/bin/git add GrantInfo_", focal.clade, "*"))
+  system(paste0("/usr/local/bin/git commit -m'Adding info for clade ", focal.clade, "'"))
+  system("/usr/local/bin/git push")
   }
 }
 
