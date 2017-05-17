ea1712b505b552d76a8d98450150b07779a4cd3f
Brian OMeara <omeara.brian@gmail.com>
2017-05-17 09:36:25 -0400

    updated figures

--- FundingSearch/CreatePlots.R
+++ FundingSearch/CreatePlots.R
@@ -23,7 +23,7 @@ aggregate.df <- data.frame()
 for (clade.index in sequence(length(clade.names))) {
     print(paste("loading", clade.names[clade.index]))
     clade.info <- read.csv(files[clade.index], stringsAsFactors=FALSE)
-    
+
     local.df <- data.frame(clade=clade.names[clade.index], number.grants=nrow(clade.info), total.grants=sum(clade.info$amount), median.grant=median(clade.info$amount), mean.grant=mean(clade.info$amount), PIs=paste(clade.info$PI, collapse=", "), all.investigators=paste(gsub("\\|", ",", clade.info$investigators), collapse=", "), stringsAsFactors=FALSE)
     summary.df <- rbind(summary.df, local.df, stringsAsFactors=FALSE)
     clade.info$taxon <- clade.names[clade.index]
@@ -37,7 +37,7 @@ for (clade.index in sequence(length(clade.names))) {
     clade.df <- subset(aggregate.df, taxon==clade.names[clade.index])
     dissertation.df <- clade.df[grepl("dissertation", clade.df$title, ignore.case=TRUE),]
     if(nrow(dissertation.df)>0) {
-        write.csv(dissertation.df, file=paste0("DissertationGrantInfo_", clade.names[clade.index], ".csv"))   
+        write.csv(dissertation.df, file=paste0("DissertationGrantInfo_", clade.names[clade.index], ".csv"))
     }
     person.info <- data.frame()
     all.people <- c()
@@ -50,7 +50,7 @@ for (clade.index in sequence(length(clade.names))) {
     money.by.person <- data.frame()
     unique.people <- unique(all.people)
     for (person.index in sequence(length(unique.people))) {
-        money.by.person <- rbind(money.by.person, data.frame(word=unique.people[person.index], freq=sum(subset(person.info, word==unique.people[person.index])$freq), stringsAsFactors=FALSE))   
+        money.by.person <- rbind(money.by.person, data.frame(word=unique.people[person.index], freq=sum(subset(person.info, word==unique.people[person.index])$freq), stringsAsFactors=FALSE))
     }
     cat(as.character(clade.names[clade.index]))
     all.people.df <- data.frame(table(all.people))
@@ -58,6 +58,8 @@ for (clade.index in sequence(length(clade.names))) {
     all.words <- VCorpus(VectorSource(removePunctuation(paste(unlist(c(clade.df$title, clade.df$abstract)), collapse=" "))))
     all.words <- tm_map(all.words, content_transformer(tolower))
     all.words <- tm_map(all.words, removeWords, stopwords("english"))
+    all.words <- tm_map(all.words, removeWords, c("will", "also"))
+
     all.words <- tm_map(all.words, stripWhitespace)
     #all.words <- tm_map(all.words, stemDocument)
     all.words.df <- data.frame(table(strsplit(as.character(all.words[[1]]), " ")[[1]]))
@@ -65,26 +67,28 @@ for (clade.index in sequence(length(clade.names))) {
     if(nrow(all.words.df)>1000) {
      all.words.df <- all.words.df[order(all.words.df$freq, decreasing=TRUE),]
      all.words.df <- all.words.df[1:1000,]
-        
+
     }
-    
+
     money.by.person <- money.by.person[order(money.by.person$freq, decreasing=TRUE),]
     all.people.df <- all.people.df[order(all.people.df$freq, decreasing=TRUE),]
-    
-    
+
+
     my_graph =     wordcloud2(all.words.df)
     saveWidget(my_graph,"tmp.html",selfcontained = F)
     webshot("tmp.html",paste0("Figure_",clade.names[clade.index],"_Words.pdf"), delay =5, vwidth = 480, vheight=480)
-    
+
+
     my_graph =     wordcloud2(money.by.person)
     saveWidget(my_graph,"tmp.html",selfcontained = F)
     webshot("tmp.html",paste0("Figure_",clade.names[clade.index],"_PeopleByMoney.pdf"), delay =5, vwidth = 480, vheight=480)
-    
 
-    
+
+
     my_graph =     wordcloud2(all.people.df)
     saveWidget(my_graph,"tmp.html",selfcontained = F)
     webshot("tmp.html",paste0("Figure_",clade.names[clade.index],"_PeopleByNumberOfGrants.pdf"), delay =5, vwidth = 480, vheight=480)
-    
-  
+
+    system("/usr/local/bin/git commit -m'updated figures' -a")
+    system("/usr/local/bin/git push")
 }
